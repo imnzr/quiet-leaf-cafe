3e82799df1cf7de303af10e4c6dcb76dde286731
Nizar Fadilah <nizarfadilah74@gmail.com>
2025-06-05 17:27:48 +0700

    add: repository query for customer

---
+++ backend/repository/customer_repository/customer_impl.go
@@ -0,0 +1,122 @@
+package customerrepository
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+
+	"github.com/imnzr/quiet-leaf-cafe/backend/helper"
+	"github.com/imnzr/quiet-leaf-cafe/backend/models"
+)
+
+type CustomerRepositoryImpl struct{}
+
+func NewCustomerRepository() CustomerRepository {
+	return CustomerRepositoryImpl{}
+}
+
+// Delete implements CustomerRepository.
+func (c CustomerRepositoryImpl) Delete(ctx context.Context, tx *sql.Tx, customer models.Customer) error {
+	query := "DELETE FROM customer WHERE customer_id = ?"
+	result, err := tx.ExecContext(ctx, query, customer.Customer_id)
+	if err != nil {
+		return fmt.Errorf("failed to delete customer: %w", err)
+	}
+
+	RowsAffected, err := result.RowsAffected()
+	if err != nil {
+		return fmt.Errorf("failed to get rows affected: %w", err)
+	}
+	if RowsAffected == 0 {
+		return fmt.Errorf("no customer found with id: %d", customer.Customer_id)
+	}
+
+	return nil
+}
+
+// FindByAll implements CustomerRepository.
+func (c CustomerRepositoryImpl) FindByAll(ctx context.Context, tx *sql.Tx) ([]models.Customer, error) {
+	query := "SELECT customer_id, name, phone_number, email FROM `customer`"
+	rows, err := tx.QueryContext(ctx, query)
+	helper.HandleQueryError(err)
+
+	defer rows.Close()
+
+	var customers []models.Customer
+	for rows.Next() {
+		customer := models.Customer{}
+		err := rows.Scan(&customer.Customer_id, &customer.Name, &customer.Phone_number, &customer.Email)
+		if err != nil {
+			fmt.Println("Error scanning row:", err)
+			continue // Skip this row if there's an error
+		}
+		customers = append(customers, customer)
+	}
+	return customers, nil
+}
+
+// FindById implements CustomerRepository.
+func (c CustomerRepositoryImpl) FindById(ctx context.Context, tx *sql.Tx, customer_id int) (models.Customer, error) {
+	query := "SELECT customer_id, name, phone_number, email FROM `customer` WHERE customer_id = ?"
+	rows, err := tx.QueryContext(ctx, query, customer_id)
+	helper.HandleQueryError(err)
+
+	defer rows.Close()
+
+	customer := models.Customer{}
+
+	if rows.Next() {
+		err := rows.Scan(&customer.Customer_id, &customer.Name, &customer.Phone_number, &customer.Email)
+		helper.HandleErrorRows(err)
+		return customer, nil
+	} else {
+		return models.Customer{}, fmt.Errorf("customer with id %d not found", customer_id)
+	}
+}
+
+// Login implements CustomerRepository.
+func (c CustomerRepositoryImpl) Login(ctx context.Context, tx *sql.Tx, customer models.Customer) (models.Customer, error) {
+	query := "SELECT customer_id, name, phone_number, email FROM `customer` WHERE email = ? AND password = ?"
+	rows, err := tx.QueryContext(ctx, query, customer.Email, customer.Password)
+	helper.HandleQueryError(err)
+
+	defer rows.Close()
+
+	if rows.Next() {
+		err := rows.Scan(&customer.Customer_id, &customer.Name, &customer.Phone_number, &customer.Email)
+		helper.HandleErrorRows(err)
+		return customer, nil
+	} else {
+		return models.Customer{}, fmt.Errorf("invalid email or password")
+	}
+}
+
+// Save implements CustomerRepository.
+func (c CustomerRepositoryImpl) Save(ctx context.Context, tx *sql.Tx, customer models.Customer) (models.Customer, error) {
+	query := "INSERT INTO customer (name, phone_number, email, password) VALUES(?, ?, ?, ?)"
+	result, err := tx.ExecContext(ctx, query, customer.Name, customer.Phone_number, customer.Email, customer.Password)
+	helper.HandleQueryError(err)
+
+	LastInsertId, err := result.LastInsertId()
+	if err != nil {
+		return models.Customer{}, fmt.Errorf("failed to get last insert id: %w", err)
+	}
+	customer.Customer_id = int(LastInsertId)
+	return customer, nil
+}
+
+// Update implements CustomerRepository.
+func (c CustomerRepositoryImpl) Update(ctx context.Context, tx *sql.Tx, customer models.Customer) (models.Customer, error) {
+	query := "UPDATE customer SET name = ?, phone_number = ?, email = ? WHERE customer_id = ?"
+	result, err := tx.ExecContext(ctx, query, customer.Name, customer.Phone_number, customer.Email, customer.Customer_id)
+	helper.HandleQueryError(err)
+
+	RowsAffected, err := result.RowsAffected()
+	if err != nil {
+		return models.Customer{}, fmt.Errorf("failed to get rows affected: %w", err)
+	}
+	if RowsAffected == 0 {
+		return models.Customer{}, fmt.Errorf("no customer found with id: %d", customer.Customer_id)
+	}
+	return customer, nil
+}
---
+++ backend/repository/customer_repository/customer_interface.go
@@ -0,0 +1,17 @@
+package customerrepository
+
+import (
+	"context"
+	"database/sql"
+
+	"github.com/imnzr/quiet-leaf-cafe/backend/models"
+)
+
+type CustomerRepository interface {
+	Save(ctx context.Context, tx *sql.Tx, customer models.Customer) (models.Customer, error)
+	Update(ctx context.Context, tx *sql.Tx, customer models.Customer) (models.Customer, error)
+	Delete(ctx context.Context, tx *sql.Tx, customer models.Customer) error
+	FindById(ctx context.Context, tx *sql.Tx, customer_id int) (models.Customer, error)
+	FindByAll(ctx context.Context, tx *sql.Tx) ([]models.Customer, error)
+	Login(ctx context.Context, tx *sql.Tx, customer models.Customer) (models.Customer, error)
+}
